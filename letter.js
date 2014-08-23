1d9d9d0d0df7ca287992ff17cd895ffa2c4e6f97
Sammy Jelin <sjelin@gmail.com>
2014-08-23 00:53:32 -0400

    v1

--- letter.js
+++ letter.js
@@ -2,8 +2,21 @@ var defaultText;//Will be lorem ipsum, but I'm setting it at the bottom
 
 function injectText(text) {
 	var paragraphs = ((text||"").trim() || defaultText).split("\n");
+
+	//Image
+	var $img = $(".their.info img");
+	$img.attr("src", paragraphs[0]);
+	var img_width_height = paragraphs[1].split("x");
+	var img_w = img_width_height[0].trim();
+	var img_h = (img_width_height[1] || "").trim();
+	if(img_w.length > 0)
+		$img.css("width", img_w);
+	if(img_h.length > 0)
+		$img.css("height", img_h);
+
+	//Text
 	var contactInfo = true;
-	for(var i = 0; i < paragraphs.length; i++) {
+	for(var i = 2; i < paragraphs.length; i++) {
 		var paragraph = paragraphs[i].trim();
 		if(paragraph.length) {
 			var $p = $("<p>");
@@ -11,7 +24,7 @@ function injectText(text) {
 			if(contactInfo)
 				$(".their.info .content").append($p);
 			else
-				$(".letter .content .signature").before($p);
+				$(".letter .content").append($p);
 		} else
 			contactInfo = false;
 	}
@@ -24,12 +37,14 @@ window.onload = function() {
 }
 
 
-defaultText =	"Lorem ipsum\n"+
+defaultText =	"data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=\n"+
+				"100%\n"+
+				"Lorem ipsum\n"+
 				"dolorsit.amet\n"+
-				"Consectetur, AE\n\n"+
+				"1234 Consectetur St.\n"+
+				"Adipiscing, EL, 56789\n\n"+
 				"Dear Lorem ipsum,\n"+
 				"Maecenas vitae odio in ipsum tempus volutpat nec non nisl. Phasellus faucibus lorem nunc, et aliquet ante interdum et. Morbi turpis risus, egestas sed eros vel, elementum vulputate turpis. Ut a dui sollicitudin, tincidunt elit ut, sagittis ipsum. Duis varius felis a metus congue dapibus. Praesent quis luctus mauris, sit amet vulputate sapien. Curabitur quis lacus sit amet quam viverra malesuada. Sed sit amet erat id purus congue blandit. Duis bibendum malesuada dolor, lobortis luctus felis eleifend nec. Mauris vitae risus nibh. Quisque tempus sem feugiat tellus blandit, in faucibus arcu placerat. Phasellus ac augue id velit aliquet rutrum. Etiam porttitor elit tristique nisi convallis, a rutrum lectus posuere.\n"+
-				"Quisque hendrerit in nunc a vulputate. Sed eu neque congue, hendrerit nunc a, dapibus ante. Pellentesque vulputate viverra lacus, eu auctor ligula fermentum vitae. Aenean ultrices blandit ultricies. Praesent sem lectus, tristique nec odio in, ornare ornare justo. Vivamus sollicitudin quam turpis, vitae commodo dolor euismod eu. Pellentesque dolor felis, ullamcorper at nisi sit amet, faucibus ultricies urna. Mauris placerat quam ipsum, ac feugiat ante bibendum cursus. Nulla dictum lacus ut sapien dignissim, sit amet convallis mi tristique. Maecenas in pellentesque turpis. Nulla imperdiet urna lectus, ac malesuada justo condimentum nec. Quisque id facilisis arcu. Donec imperdiet lorem a dolor suscipit fringilla.\n"+
+				"Quisque hendrerit in nunc a vulputate.  Pellentesque vulputate viverra lacus, eu auctor ligula fermentum vitae. Aenean ultrices blandit ultricies. Praesent sem lectus, tristique nec odio in, ornare ornare justo. Vivamus sollicitudin quam turpis, vitae commodo dolor euismod eu. Pellentesque dolor felis, ullamcorper at nisi sit amet, faucibus ultricies urna. Mauris placerat quam ipsum, ac feugiat ante bibendum cursus. Nulla dictum lacus ut sapien dignissim, sit amet convallis mi tristique. Maecenas in pellentesque turpis. Nulla imperdiet urna lectus, ac malesuada justo condimentum nec. Quisque id facilisis arcu. Donec imperdiet lorem a dolor suscipit fringilla.\n"+
 				"Vivamus vitae lacinia neque, eu rutrum mauris. Donec sit amet nibh ac urna faucibus lobortis. Fusce scelerisque aliquam tincidunt. Fusce tempus libero ac risus blandit, nec consectetur sapien tincidunt. Proin lacinia mauris non mi tempus rhoncus. Phasellus commodo nibh quis hendrerit aliquam. Pellentesque sit amet eros purus. Phasellus sit amet blandit orci. Nulla felis tellus, vestibulum ac libero id, vulputate adipiscing turpis. Mauris ac consequat tellus.\n"+
-				"Etiam condimentum, dolor non blandit gravida, odio leo fringilla ante, quis pharetra turpis dui a odio. Mauris id nisi ultrices, eleifend erat id, varius nulla. Sed id eros suscipit, fermentum nibh sit amet, tempus neque. Etiam convallis est ac purus varius interdum. Nam euismod lacus a odio cursus rhoncus. Nunc molestie elementum dolor, in molestie tortor tristique quis. In ut arcu nisl. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et turpis accumsan, egestas sapien nec, vehicula sem. Nam sodales, dolor vitae auctor porttitor, justo tortor ornare arcu, id blandit nunc felis ut est. Donec posuere urna enim, id sodales sapien suscipit at. Quisque tristique eget purus eu consectetur. Curabitur at magna ac metus tempor gravida id ac enim. Fusce sem lacus, lacinia semper nulla feugiat, ullamcorper mattis augue. Nunc eget est massa.\n"+
 				"Nulla quis consectetur leo. Curabitur suscipit pellentesque augue a pulvinar. Morbi aliquam ligula in mauris fermentum, nec pharetra neque posuere. Nullam eget euismod nulla, eu tincidunt eros. Curabitur luctus ac felis nec sagittis. Proin feugiat sollicitudin vulputate. Sed iaculis urna turpis, quis euismod mi pharetra ac.";
