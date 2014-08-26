856895f576a42f90e30f5eacf8d1a6b146369907
Sammy Jelin <sjelin@gmail.com>
2014-08-26 15:22:26 -0400

    better formatting & defaults

--- letter.js
+++ letter.js
@@ -1,7 +1,13 @@
 var defaultText;//Will be lorem ipsum, but I'm setting it at the bottom
 
+transGif = "data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7";
+
 function injectText(text) {
 	var paragraphs = ((text||"").trim() || defaultText).split("\n");
+
+	if((paragraphs[0].indexOf("://") == -1) &&
+			(paragraphs[0].indexOf("data:") == -1))
+		paragraphs.unshift(transGif, "");//Add image info
 
 	//Image
 	var $img = $(".their.info img");
@@ -14,18 +20,24 @@ function injectText(text) {
 	if(img_h.length > 0)
 		$img.css("height", img_h);
 
+	//Date
+	var d = new Date();
+	$(".date").text(["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][d.getMonth()] + " " + d.getDate() + ", " + (1990 + d.getYear()));
+
 	//Text
 	var contactInfo = true;
+	var started = false;
 	for(var i = 2; i < paragraphs.length; i++) {
 		var paragraph = paragraphs[i].trim();
 		if(paragraph.length) {
+			started = true;
 			var $p = $("<p>");
 			$p.text(paragraph);
 			if(contactInfo)
 				$(".their.info .content").append($p);
 			else
 				$(".letter .content").append($p);
-		} else
+		} else if(started)
 			contactInfo = false;
 	}
 }
@@ -44,7 +56,7 @@ defaultText =	"data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQ
 				"1234 Consectetur St.\n"+
 				"Adipiscing, EL, 56789\n\n"+
 				"Dear Lorem ipsum,\n"+
-				"Maecenas vitae odio in ipsum tempus volutpat nec non nisl. Phasellus faucibus lorem nunc, et aliquet ante interdum et. Morbi turpis risus, egestas sed eros vel, elementum vulputate turpis. Ut a dui sollicitudin, tincidunt elit ut, sagittis ipsum. Duis varius felis a metus congue dapibus. Praesent quis luctus mauris, sit amet vulputate sapien. Curabitur quis lacus sit amet quam viverra malesuada. Sed sit amet erat id purus congue blandit. Duis bibendum malesuada dolor, lobortis luctus felis eleifend nec. Mauris vitae risus nibh. Quisque tempus sem feugiat tellus blandit, in faucibus arcu placerat. Phasellus ac augue id velit aliquet rutrum. Etiam porttitor elit tristique nisi convallis, a rutrum lectus posuere.\n"+
+				"Maecenas vitae odio in ipsum tempus volutpat nec non nisl. Phasellus faucibus lorem nunc, et aliquet ante interdum et. Morbi turpis risus, egestas sed eros vel, elementum vulputate turpis. Ut a dui sollicitudin, tincidunt elit ut, sagittis ipsum. Duis varius felis a metus congue dapibus. Praesent quis luctus mauris, sit amet vulputate sapien. Curabitur quis lacus sit amet quam viverra malesuada.\n"+
 				"Quisque hendrerit in nunc a vulputate.  Pellentesque vulputate viverra lacus, eu auctor ligula fermentum vitae. Aenean ultrices blandit ultricies. Praesent sem lectus, tristique nec odio in, ornare ornare justo. Vivamus sollicitudin quam turpis, vitae commodo dolor euismod eu. Pellentesque dolor felis, ullamcorper at nisi sit amet, faucibus ultricies urna. Mauris placerat quam ipsum, ac feugiat ante bibendum cursus. Nulla dictum lacus ut sapien dignissim, sit amet convallis mi tristique. Maecenas in pellentesque turpis. Nulla imperdiet urna lectus, ac malesuada justo condimentum nec. Quisque id facilisis arcu. Donec imperdiet lorem a dolor suscipit fringilla.\n"+
 				"Vivamus vitae lacinia neque, eu rutrum mauris. Donec sit amet nibh ac urna faucibus lobortis. Fusce scelerisque aliquam tincidunt. Fusce tempus libero ac risus blandit, nec consectetur sapien tincidunt. Proin lacinia mauris non mi tempus rhoncus. Phasellus commodo nibh quis hendrerit aliquam. Pellentesque sit amet eros purus. Phasellus sit amet blandit orci. Nulla felis tellus, vestibulum ac libero id, vulputate adipiscing turpis. Mauris ac consequat tellus.\n"+
 				"Nulla quis consectetur leo. Curabitur suscipit pellentesque augue a pulvinar. Morbi aliquam ligula in mauris fermentum, nec pharetra neque posuere. Nullam eget euismod nulla, eu tincidunt eros. Curabitur luctus ac felis nec sagittis. Proin feugiat sollicitudin vulputate. Sed iaculis urna turpis, quis euismod mi pharetra ac.";
